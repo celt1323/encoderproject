70b760952a64495efadc9b5413a79ab7ecbe1017
Celeste Tan <houseoftan4@gmail.com>
2016-10-11 09:17:17 -0700

    added keycodes for all alphabets

--- encoder.js
+++ encoder.js
@@ -2,8 +2,109 @@
   if (e.keyCode == 65) {
    var letters = $("#inputArea").val();
    $("#textarea").append(letters);
-  }
+ }
+ if (e.keyCode == 66) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 67) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 68) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
  });
+ if (e.keyCode == 69) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 70) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 71) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 72) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 73) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 74) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 75) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 76) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 77) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 78) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 79) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+
+ if (e.keyCode == 80) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 81) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 82) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 83) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 84) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 85) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 86) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 87) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 88) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 89) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+ if (e.keyCode == 90) {
+  var letters = $("#inputArea").val();
+  $("#textarea").append(letters);
+ }
+  });
 
  $("input[type='radio']").click(function(){
    $("input:checked").prop('checked',false);
