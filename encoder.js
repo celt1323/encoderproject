c556ff814b3f1a6ecd060e4086471946dbf510d8
Celeste Tan <houseoftan4@gmail.com>
2016-10-11 09:11:33 -0700

    added hieroglyphic image

--- encoder.js
+++ encoder.js
@@ -1,11 +1,24 @@
-$(document).keydown(function(e) {
- if (e.keyCode == 65) {
-  var letters = $("#inputArea").val();
-  $("#textarea").append(letters);
- }
-});
+ $(document).keydown(function(e) {
+  if (e.keyCode == 65) {
+   var letters = $("#inputArea").val();
+   $("#textarea").append(letters);
+  }
+ });
 
-$("input[type='radio']").click(function(){
-  $("input:checked").prop('checked',false);
-  $(this).prop('checked',true);
-});
+ $("input[type='radio']").click(function(){
+   $("input:checked").prop('checked',false);
+   $(this).prop('checked',true);
+ });
+
+ if ($("input:checked").val() == "echo"){
+   letters = $("#inputArea").val();
+   $("#textarea").append(letters);
+ }
+
+ if ($("input:checked").val() == "caesarcipher"){
+
+ }
+
+ if ($("input:checked").val() == "hieroglyphics"){
+
+ }
